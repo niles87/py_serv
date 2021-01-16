7d8b32e49c645b68ce16961d4761bac4b88c0369
Niles87 <nbing19@gmail.com>
2021-01-15 22:17:43 -0800

    added temp user model

--- flask_api.py
+++ flask_api.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
@@ -8,6 +8,19 @@ def index():
     return {
         "message": "App is awake"
     }
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////tmp/text.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return '<User %r>' % self.username
 
 
 if __name__ == '__main__':
